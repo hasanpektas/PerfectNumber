820e52dada6d710c7bc739eef7a68a1f38bd4d0f
hasanpektas <121904493+hasanpektas@users.noreply.github.com>
2023-04-08 21:14:47 +0300

    Find super number or not

---
+++ SuperNumber.py
@@ -0,0 +1,13 @@
+list = []
+number = int(input("Please enter a number:"))
+sum = 0 - (number)
+
+for i in range(1,number+1):
+    if number % i == 0:
+        list.append(i)
+        sum += i
+
+if number == sum:
+    print("This is a super number!")
+else:
+    print("This is not a super number!")
