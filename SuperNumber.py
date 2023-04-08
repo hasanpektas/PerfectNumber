1d729f8aa71781b0f20c84cc2b0f958457178e7e
hasanpektas <121904493+hasanpektas@users.noreply.github.com>
2023-04-08 21:18:13 +0300

    Update SuperNumber.py

--- SuperNumber.py
+++ SuperNumber.py
@@ -1,8 +1,8 @@
 list = []
 number = int(input("Please enter a number:"))
-sum = 0 - (number)
+sum = 0
 
-for i in range(1,number+1):
+for i in range(1,number-1):
     if number % i == 0:
         list.append(i)
         sum += i
